b0d1f1f0d5e706021c5e3bb9dcfc0ecc73c83e33
robin widjaja <robinsalim@yahoo.com>
2020-06-11 17:54:56 +0700

    drawer in the making

---
+++ screens/ProductsScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text, stylesheet } from "react-native";
+
+export function ProductsScreen() {
+  return (
+    <View>
+      <Text>This is ProductsScreen</Text>
+    </View>
+  );
+}
