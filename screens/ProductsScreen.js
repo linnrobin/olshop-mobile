f045842795484f7e9d2405da73cb7284ed0de780
robin widjaja <robinsalim@yahoo.com>
2020-06-12 20:59:17 +0700

    added product page

--- screens/ProductsScreen.js
+++ screens/ProductsScreen.js
@@ -1,11 +1,62 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { ScrollView, View, Text, StyleSheet, Card, Image } from "react-native";
+import { FlatList } from "react-native-gesture-handler";
 
 export default function ProductsScreen({ navigation }) {
+  const productList = [
+    {
+      image:
+        "https://media.missguided.com/s/missguided/CL110615_set/1/pink-floral-leopard-print-cupped-maxi-dress",
+      code: "98002",
+      name: "nco rajut",
+      price: 60000,
+      stock: 11,
+    },
+    {
+      image: "https://cf.shopee.co.id/file/89645841ebbd76c9849fcd5950a9de0a",
+      code: "8230",
+      name: "nco rajut",
+      price: 120000,
+      stock: 34,
+    },
+    {
+      image: "https://media.monsoon.co.uk/medias/sys_master/9449009119262.jpg",
+      code: "1911",
+      name: "nco jaket jeans",
+      price: 100000,
+      stock: 15,
+    },
+    {
+      image: "https://media.monsoon.co.uk/medias/sys_master/9449009119262.jpg",
+      code: "1911",
+      name: "nco jaket jeans",
+      price: 100000,
+      stock: 15,
+    },
+  ];
   return (
-    <View style={styles.main}>
-      <Text>This is ProductsScreen</Text>
-    </View>
+    <ScrollView>
+      <FlatList
+        numColumns={2}
+        keyExtractor={(item, index) => index}
+        renderItem={productList.map((el) => (
+          <View style={styles.main}>
+            <Image
+              style={{
+                width: 200,
+                height: 200,
+                resizeMode: "contain",
+                borderRadius: 20,
+              }}
+              source={{
+                uri: productList[2].image,
+              }}
+            />
+            <Text>{productList[2].name}</Text>
+          </View>
+        ))}
+      ></FlatList>
+    </ScrollView>
   );
 }
 
@@ -14,5 +65,8 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+    paddingTop: 20,
+    flexDirection: "row",
+    flexWrap: "wrap",
   },
 });
